8bcb81ae4a439a508064889ceaf13822fee79f2f
Dziucha <qazwsxedcrfvtgbyhnujmikolp@buziaczek.pl>
2023-02-14 13:38:25 +0100

    sending e-mail

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 import re
+import smtplib
+import ssl
 
 regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 
@@ -11,4 +13,15 @@ def check_email(email):
         confirmation_message = "Invalid e-mail address, please correct."
     return confirmation_message
 
-def send_email()
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "edyta.zelazny01@gmail.com"
+    password = "rumgavdnbjwnaljf"
+    context = ssl.create_default_context()
+    receiver = username
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
