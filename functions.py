4dafd8492f6d23accbbe1b29fd6de4a1b2cb6f07
Dziucha <qazwsxedcrfvtgbyhnujmikolp@buziaczek.pl>
2023-02-14 13:28:29 +0100

    check email function

---
+++ functions.py
@@ -0,0 +1,14 @@
+import re
+
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+
+
+def check_email(email):
+    if re.fullmatch(regex, email):
+        confirmation_message = "Thank you for sending your message."
+    else:
+        print("Invalid e-mail")
+        confirmation_message = "Invalid e-mail address, please correct."
+    return confirmation_message
+
+def send_email()
--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,8 +1,17 @@
 import streamlit as st
+import functions
 
 with st.form(key="email"):
-    st.text_input("Your e-mail address:")
+    user_email = st.text_input("Your e-mail address:")
     topics = ["Job Inquiries", "Project Proposals", "Other"]
-    st.selectbox("What topic do you want to discuss?", topics)
-    st.text_area("Your message:")
-    st.form_submit_button("Submit")
+    selected_topic = st.selectbox("What topic do you want to discuss?", topics)
+    user_message = st.text_area("Your message:")
+    submit_button = st.form_submit_button("Submit")
+
+email_check = functions.check_email(user_email)
+
+if submit_button:
+    if email_check == "Invalid e-mail address, please correct.":
+        status_message = st.info(email_check)
+    else:
+        status_message = st.info(email_check)
